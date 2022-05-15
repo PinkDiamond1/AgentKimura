4a708993383c1e3c7db25c8bf6e61f6a189f8efa
shuma <kazuma.matsuoka@hashport.io>
2022-05-15 23:43:50 +0900

    add bootstrap

--- src/index.py
+++ src/index.py
@@ -1,29 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return """
-    <head>
-    <script src="./renderer.js"></script>
-    </head>
-    <body>
-<h1><font color="red">PythonとElectronを使ったアプリ</font></h1>
-<h2><font color="blue">表示結果</font></h2>
-<p>この文章は、</p>
-<p>Electronでindex.jsを読み込んだあと、</p>
-<p><pre>「require('child_process').spawn('python',['./index.py']);」</pre></p>
-<p>によって、index.pyファイルを読み込み表示した文章です。</p>
-<form action="/" method='GET'>
-    <input type="submit" value="REFRESH">
-</form>
-</body>
+    return render_template("index.html")
 
 
-
-"""
+@app.route("/bootstrap")
+def bootstrap():
+    return  render_template("bootstrap.html")
 
 
 if __name__ == "__main__":
